c48d6f1818c49c4c08eb1b14f2894659f7f13949
exulansis1420 <tanishqd1420@gmail.com>
2021-07-09 02:37:22 +0530

    added test images

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import util
+# import util
 
 app = Flask(__name__)
 
---
+++ server/util.py
@@ -0,0 +1,6 @@
+import joblib
+import json
+import numpy as np
+import base64
+import cv2
+from wavelet import w2d
