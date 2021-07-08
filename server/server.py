70e9d75bed14afddea4995172879057e8ca8e8d7
exulansis1420 <tanishqd1420@gmail.com>
2021-07-09 02:17:29 +0530

    setup test server

---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return "hi"
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    app.run(port=5000)
