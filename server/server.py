fa4b6f551cd8a7ba8f44881ee7b5d8c7569d8fb8
exulansis1420 <tanishqd1420@gmail.com>
2021-07-12 01:27:29 +0530

    working util.py

--- server/server.py
+++ server/server.py
@@ -4,11 +4,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/hello')
-def hello():
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
     return "hi"
 
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
     app.run(port=5000)
+
